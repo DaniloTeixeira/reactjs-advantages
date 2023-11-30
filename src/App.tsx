c3e4f5e638508adfa6c707f91b1a67fcc83ba715
Danilo Teixeira <danilo.rt@outlook.com>
2023-11-30 00:20:06 -0300

    ending project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,67 @@
 import { useState } from 'react';
 import './App.scss';
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { content } from './data/content';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activeContentIndex, setActiveContentIndex] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={ reactLogo } className="logo" alt="React logo" />
         </a>
+        <div>
+          <header>
+            <div>
+              <h1>React.js</h1>
+              <p>Using the React library for rendering the UI</p>
+            </div>
+          </header>
+
+          <div id="tabs">
+            <menu>
+              <button
+                className={ activeContentIndex === 0 ? "active" : "" }
+                onClick={ () => setActiveContentIndex(0) }
+              >
+                Why React?
+              </button>
+
+              <button
+                className={ activeContentIndex === 1 ? "active" : "" }
+                onClick={ () => setActiveContentIndex(1) }
+              >
+                Core Features
+              </button>
+
+              <button
+                className={ activeContentIndex === 2 ? "active" : "" }
+                onClick={ () => setActiveContentIndex(2) }
+              >
+                Related Resources
+              </button>
+
+              <button
+                className={ activeContentIndex === 3 ? "active" : "" }
+                onClick={ () => setActiveContentIndex(3) }
+              >
+                React vs Vanilla JS
+              </button>
+            </menu>
+            <div id="tab-content">
+              <ul>
+                { content[activeContentIndex].map((item) => (
+                  <li key={ item }>{ item }</li>
+                )) }
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
